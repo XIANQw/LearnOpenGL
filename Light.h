67f0d71e43ebf0341dbae06f8c6cafe6964c324f
XIANQw <qiwei-xian@qq.com>
2021-03-22 15:28:58 +0800

    Abstract light

---
+++ Light.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "GameObj.h"
+
+namespace myLight {
+
+	const glm::vec3 DEFAULT_KA = glm::vec3(0.2f);
+	const glm::vec3 DEFAULT_KD = glm::vec3(0.5f);
+	const glm::vec3 DEFAULT_KS = glm::vec3(1.0f);
+
+	struct Light {
+		glm::vec3 ambient;
+		glm::vec3 diffuse;
+		glm::vec3 specular;
+
+		GameObj* obj;
+		Light() :ambient(DEFAULT_KA), diffuse(DEFAULT_KD), specular(DEFAULT_KS), obj(nullptr) {}
+	};
+
+	struct PointLight : public Light {
+		glm::vec3 position;
+	};
+
+
+}
